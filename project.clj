1a1767fb2743586dacf93a6bddd9fca3ccf5ab20
Misha Vakulich <m.vakulich@softteco.com>
2023-08-19 23:16:02 +0300

    upgrade libe dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject pubsure/pubsure-ws "0.1.0-SNAPSHOT"
+(defproject pubsure/pubsure-ws "0.2.0-SNAPSHOT"
   :description "WAMP over Websocket implementation for pubsure."
   :url "https://github.com/touch/pubsure-ws"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [pubsure/pubsure-core "0.1.0-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [pubsure/pubsure-core "0.2.0-SNAPSHOT"]
+                 [org.clojure/core.async "1.5.640"]
                  [http-kit "2.1.18"]
                  [clj-wamp "1.0.2"]
                  [cheshire "5.4.0"]
                  [org.clojure/data.codec "0.1.0"]
-                 [com.taoensso/timbre "3.1.6"]
+                 [com.taoensso/timbre "6.2.2"]
                  [org.clojure/core.cache "0.6.4"]]
   :profiles {:test {:dependencies [[stylefruits/gniazdo "0.4.0"]]}}
   :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.15"
