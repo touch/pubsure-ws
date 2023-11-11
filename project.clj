d0ae1480c600ae18e8d15d0c24da91524e184a5e
Misha Vakulich <m.vakulich@softteco.com>
2023-11-12 01:57:38 +0300

    up clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/touch/pubsure-ws"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [pubsure/pubsure-core "0.2.0-SNAPSHOT"]
                  [org.clojure/core.async "1.5.640"]
                  [http-kit "2.1.18"]
